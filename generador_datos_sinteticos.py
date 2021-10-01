77840156d5a8efc71efbee71170d44a696bc3050
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2021-10-01 20:38:01 -0300

    funciona con logica

--- generador_datos_sinteticos.py
+++ generador_datos_sinteticos.py
@@ -5,9 +5,9 @@ import pandas as pd
 class Decay:
 	noise = lambda self, arr,x:np.random.randn(arr.shape[0])*x
 	def __init__ (self, timeframe=20, concentracion_inicial=1):
-		self.tiempo=np.linspace(.001,timeframe, 70)
+		self.tiempo=np.linspace(.001,timeframe*2, 70)
 		self.constante=((1/timeframe)*1.1 - (1/timeframe)*.9) * np.random.random() + 1/timeframe*.9
-		self.sigma=.03
+		self.sigma=.01
 		self.concentracion_inicial=concentracion_inicial
 	def orden_cero(self):
 		cinetica = self.concentracion_inicial - self.constante*self.tiempo + self.noise(self.tiempo,self.sigma)
@@ -23,7 +23,7 @@ def main():
 	decay=Decay()
 	kins=[decay.orden_cero(), decay.orden_uno(), decay.orden_dos()]
 	choice=np.random.choice(range(len(kins)))
-	chosen_kin=kins[choice]
+	chosen_kin=kins.pop(choice)
 	df=pd.DataFrame(chosen_kin, columns=['tiempo (m)', 'concentración'])
 	return df,choice
 
