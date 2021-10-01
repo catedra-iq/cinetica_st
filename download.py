f194b3d56d6c1d570bef6cf8580733d9e7a618a3
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2021-10-01 18:48:25 -0300

    cambio el nombre del archivo descargado

--- download.py
+++ download.py
@@ -7,5 +7,5 @@ def get_table_download_link(df):
     """
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}"download="practica.csv">Descargar archivo</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="practica.csv">Descargar archivo</a>'
     return href
