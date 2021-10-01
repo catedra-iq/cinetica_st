6730011657ecc37acb5af5a354e8deb6a0d1cbc2
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2021-10-01 20:38:59 -0300

    removi indicacion de orden de reacción

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import download
 st.title('Práctica de Cinética')
 df,i=generador_datos_sinteticos.main()
 st.write(df)
-st.write(i)
+# st.write(i)
 st.markdown(download.get_table_download_link(df,i), unsafe_allow_html=True)
