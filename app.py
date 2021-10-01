872d1d2e2590cf254ede77f446920f0f21c4d93c
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2021-10-01 19:04:05 -0300

    fix again

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import  generador_datos_sinteticos
 import download
 st.title('Práctica de Cinética')
-df=generador_datos_sinteticos.main()
-st.write(df[0])
-st.markdown(download.get_table_download_link(df), unsafe_allow_html=True)
+df,i=generador_datos_sinteticos.main()
+st.write(df)
+st.markdown(download.get_table_download_link(df,i), unsafe_allow_html=True)
